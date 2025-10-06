378ccb0ac65b2eceb7b4a22b7c1c45857b8574ea
user <user@WIN-LNPL3JHKMJS>
2025-10-06 22:27:39 +0300

    Добавьте файлы проекта.

---
+++ Source.c
@@ -0,0 +1,28 @@
+﻿#include <stdio.h>
+#include <locale.h>
+
+int main()
+{
+    setlocale(LC_ALL, "RUS");
+
+    int A, B, C, sum;
+
+    printf("Введите номер игрока A: ");
+    scanf("%d", &A);
+    printf("Введите номкр игрока B: ");
+    scanf("%d", &B);
+    printf("Введите номер игрока C: ");
+    scanf("%d", &C);
+
+    sum = A + B + C;
+    if (sum % 3 == 0)
+    {
+        printf("Тройка игроков с номерами %d, %d, %d является счастливой!\n", A, B, C);
+    }
+    else
+    {
+        printf("Тройка игроков с номерами %d, %d, %d НЕ является счастливой.\n", A, B, C);
+    }
+
+    return 0;
+}
